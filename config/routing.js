941a1a50bbbb5047a0ac75109f63d13360b76ed6
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2018-06-13 15:19:28 -0300

    ajustado caminho de views

--- config/routing.js
+++ config/routing.js
@@ -6,15 +6,15 @@
             $routeProvider
                 .when("/", {
                     controller: "homeController",
-                    templateUrl: "../views/home.html"
+                    templateUrl: "views/home.html"
                 })
                 .when("/alterar-nome", {
                     controller: "alterarNomeController",
-                    templateUrl: "../views/alterar-nome.html"
+                    templateUrl: "views/alterar-nome.html"
                 })
                 .when("/nova-todo", {
                     controller: "novaTodoController",
-                    templateUrl: "../views/nova-todo.html"
+                    templateUrl: "views/nova-todo.html"
                 })
                 
                 .otherwise({redirectTo: '/'});
