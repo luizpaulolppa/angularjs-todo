01a6b975644a887c4b34abb9b073f24ce4245f6f
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2018-06-13 18:16:16 -0300

    ajustes

--- public/services/todosService.js
+++ public/services/todosService.js
@@ -6,7 +6,16 @@
 
     function todosService() {
 
-        var getTodos = function() {
+        function getUUID() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+        }
+
+        var getTodos = function () {
             var todos = localStorage.getItem("todos");
             if (todos) {
                 todos = JSON.parse(todos);
@@ -16,15 +25,46 @@
             return todos;
         }
 
-        var postTodo = function(todo) {
+        var postTodo = function (todo) {
             var todos = getTodos();
+            todo.id = getUUID();
             todos.push(todo);
             localStorage.setItem("todos", JSON.stringify(todos));
         }
 
+        var deleteTodo = function (id) {
+            var todos = getTodos();
+            if (todos && todos.length) {
+                for (var t = 0; t < todos.length; t++) {
+                    var todo = todos[t];
+                    if (todo.id === id) {
+                        var indiceTodo = todos.indexOf(todo);
+                        todos.splice(indiceTodo, 1);
+                        localStorage.setItem("todos", JSON.stringify(todos));
+                    }
+                }
+            }
+        }
+
+        var checkTodo = function (id) {
+            var todos = getTodos();
+            if (todos && todos.length) {
+                for (var t = 0; t < todos.length; t++) {
+                    var todo = todos[t];
+                    if (todo.id === id) {
+                        todo.concluida = !todo.concluida;
+                    }
+                }
+
+                localStorage.setItem("todos", JSON.stringify(todos));
+            }
+        }
+
         return {
             getTodos: getTodos,
-            postTodo: postTodo
+            postTodo: postTodo,
+            deleteTodo: deleteTodo,
+            checkTodo: checkTodo
         }
     }
 
